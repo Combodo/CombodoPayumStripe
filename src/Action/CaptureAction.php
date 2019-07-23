6c1368f64b5d93e41a77a75d221734f4e478b210
PyRowMan <vcorre@pielectronique.com>
2019-07-23 15:14:37 +0200

    Update CaptureAction.php

--- src/Action/CaptureAction.php
+++ src/Action/CaptureAction.php
@@ -1,6 +1,7 @@
 <?php
 namespace Combodo\StripeV3\Action;
 
+use Combodo\StripeV3\Request\Api\CheckPayment;
 use Payum\Core\Action\ActionInterface;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\GatewayAwareInterface;
@@ -9,6 +10,8 @@ use Payum\Core\Request\Capture;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Combodo\StripeV3\Request\Api\CreateCharge;
 use Combodo\StripeV3\Request\Api\ObtainToken;
+use Payum\Core\Request\GetHttpRequest;
+use Stripe\Stripe;
 
 class CaptureAction implements ActionInterface, GatewayAwareInterface
 {
@@ -24,7 +27,14 @@ class CaptureAction implements ActionInterface, GatewayAwareInterface
         RequestNotSupportedException::assertSupports($this, $request);
 
         $model = ArrayObject::ensureArrayObject($request->getModel());
-
+        $getHttpRequest = new GetHttpRequest();
+        $this->gateway->execute($getHttpRequest);
+        if ($getHttpRequest->method == 'GET' && isset($getHttpRequest->query['status'])) {
+            $obtainToken = new CheckPayment($request->getToken());
+            $obtainToken->setModel($model);
+            $this->gateway->execute($obtainToken);
+            return;
+        }
         if ($model['status']) {
             return;
         }
